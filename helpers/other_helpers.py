68af6f931e08d7f893d5d74bc17cb81458392199
Sander Jenk <sander.jenk@gmail.com>
2022-02-13 23:57:36 +0200

    add cosine similarity function

--- helpers/other_helpers.py
+++ helpers/other_helpers.py
@@ -1,5 +1,9 @@
 import numpy as np
+from scipy import spatial
 
 def get_individuals_by_bit_array(df, bit_array):
     indices = np.array(bit_array == 1)
     return df.iloc[indices]
+
+def cosine_similarity(vector1, vector2):
+    return 1 - spatial.distance.cosine(vector1, vector2)
