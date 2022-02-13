8da6bbf683c81d17bfe590077578744e1e82df9d
Sander Jenk <sander.jenk@gmail.com>
2022-02-13 23:58:53 +0200

    refac

--- helpers/algorithm.py
+++ helpers/algorithm.py
@@ -17,29 +17,27 @@ if __name__ == '__main__':
 
 	backlog = pp.get_backlog_issues(dataset)
 
-	number_of_topics = 10
+	number_of_topics = 5
 
 	lda_model, dictionary = lda.get_lda_model(done, number_of_topics)
 
 	# print topics
-	# for idx, topic in lda_model.print_topics(-1):
-	# 	print('Topic: {} \nWords: {}'.format(idx, topic))
+	for idx, topic in lda_model.print_topics(-1):
+		print('Topic: {} \nWords: {}'.format(idx, topic))
 
 	backlog = lda.add_topic_vector_to_baclog_issues(backlog, lda_model, dictionary, number_of_topics)
 
-	backlog = backlog.iloc[:10]
+	backlog = backlog.iloc[:100]
 
-	print(backlog[["storypoints", "businessvalue"]].head(10))
-	
 	user_df = lda.add_experience_topic_vector_to_users(done, lda_model, dictionary, number_of_topics)
 
-	res = nsga2.get_optimization_result(backlog, 5)
+	res = nsga2.get_optimization_result(backlog, 15)
 
-	best_solution_indices =  res.X.astype(int)
+	best_solution_indices = res.X.astype(np.int)
 
-	best_solution = h.get_individuals_by_bit_array(backlog, best_solution_indices)
+	best_solution = h.get_individuals_by_bit_array(backlog, best_solution_indices[0])
 
-	print("Best solution indices: ", best_solution_indices)
-	print("Best solution: ", best_solution.head())
-	print("Some important variable: ", -res.F)
-	print("Is valid solution: ", res.pf)
+	print("Best solution found: %s" % res.X.astype(int))
+	print("Best solution values: ", best_solution[["storypoints", "businessvalue"]].head(20))
+	print("Function value: %s" % res.F)
+	print("Constraint violation: %s" % res.CV)
