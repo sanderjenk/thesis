34df538f71af4b1a921678086372e6bc583e68b8
Sander Jenk <sander.jenk@gmail.com>
2022-03-08 21:01:43 +0200

    add hyperparameters method

--- thesis-api/algorithm/helpers/other_helpers.py
+++ thesis-api/algorithm/helpers/other_helpers.py
@@ -1,17 +1,37 @@
+import pandas as pd
 import numpy as np
 from scipy import spatial
 
 def get_done_issues(df):
+
 	df = df.loc[(df["assignee.name"].notna()) & (df["resolutiondate"].notna())]
+
 	return df
 
 def get_backlog_issues(df):
+
 	df = df.loc[(df["assignee.name"].isna()) & (df["resolutiondate"].isna())]
+
 	return df
 
 def get_individuals_by_bit_array(df, bit_array):
+
     indices = np.array(bit_array == 1)
+
     return df.iloc[indices]
 
 def cosine_similarity(vector1, vector2):
+
     return 1 - spatial.distance.cosine(vector1, vector2)
+
+def get_hyperparameters(project):
+
+    df = pd.read_csv('./lda_tuning_results/' + project + '_lda_tuning_results.csv', encoding='utf-8')
+
+    index = df['Coherence'].idxmax()
+
+    row = df.iloc[index]
+    
+    return row["Topics"], row["Alpha"], row["Beta"]
+
+
