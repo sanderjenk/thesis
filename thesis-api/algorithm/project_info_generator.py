00aa41cd1084f2ceea57cc5fb67189e64e7875a2
Sander Jenk <sander.jenk@gmail.com>
2022-03-08 21:02:21 +0200

    project info generation script

---
+++ thesis-api/algorithm/project_info_generator.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+if __name__ == '__main__':
+	issues = pd.read_csv('./dataset/jiradataset_issues_v1.4.csv', encoding='utf-8')
+
+	projects = issues.groupby("project")
+	
+	project_names = []
+	for project, _ in projects: 
+     
+		project_names.append(project.upper())
+  
+	projects = pd.read_csv("./dataset/JIRA project list.csv", delimiter=";")
+
+	projects = projects[projects['Project Key'].isin(project_names)]
+ 
+	projects = projects[["Project Key", "Description", "Jira URL", "Git URL" ,"Programing language", "Purpose", "Reference", "Developer"]]
+ 
+	pd.DataFrame(projects).to_csv('./dataset/project_info.csv', index=False)
