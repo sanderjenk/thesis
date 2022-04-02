cec6bf0a78452f29acd26eb5960ac74cd9a680c6
Sander Jenk <sander.jenk@gmail.com>
2022-04-03 00:19:21 +0300

    new research

--- thesis-api/algorithm/research.py
+++ thesis-api/algorithm/research.py
@@ -1,4 +1,3 @@
-from operator import ge
 import pandas as pd
 import helpers.lda as lda
 import helpers.nsga2 as nsga2
@@ -12,9 +11,9 @@ from pymoo.optimize import minimize
 from pymoo.algorithms.moo.nsga2 import NSGA2
 import tqdm
 import glob
+import time
 
 pd.options.mode.chained_assignment = None  # default='warn'
-
 
 def get_generations():
     gens = {
@@ -37,23 +36,20 @@ def get_generations():
     return gens
 
 
-def get_hypervolume(backlog, user_experience_vector, storypoints, generations):
+def get_hypervolume(backlog, user_experience_vector, storypoints):
 
 	backlog["issue_similarity"] = backlog.apply(lambda x: h.cosine_similarity_with_intersection(user_experience_vector, x["vector"]), axis = 1)
  
 	backlog["novelty"] = backlog.apply(lambda x: h.calculate_novelty(user_experience_vector, x["vector"]), axis = 1)
  
-	businessvalue_array = backlog["businessvalue_normalized"].to_numpy()
- 
-	storypoints_array = backlog["storypoints"].to_numpy()
- 
+	businessvalue_array = backlog["businessvalue"].to_numpy()
+  
 	issue_similarity_array = backlog["issue_similarity"].to_numpy()
  
 	novelty_array = backlog["novelty"].to_numpy()
  
-	problem = nsga2.get_problem(storypoints_array, businessvalue_array, issue_similarity_array, novelty_array, storypoints)
+	problem = nsga2.get_problem(businessvalue_array, issue_similarity_array, novelty_array, storypoints)
 
- 
 	algorithm = NSGA2(
 		pop_size=200,
 		sampling=get_sampling("bin_random"),
@@ -63,7 +59,8 @@ def get_hypervolume(backlog, user_experience_vector, storypoints, generations):
   	)
 
 	res = minimize(problem,
-		algorithm, HVTermination(),
+		algorithm, 
+  		HVTermination(10, 0.001),
 		verbose=False,
   		save_history=True)
  
@@ -82,15 +79,15 @@ def save_results():
 		
 		if (project == "MDL"):
 			continue
-		
 
 		print(project)
+  
+		lda_start_time = time.time()
+  
 		
 		done = h.get_done_issues(project_df)
 
 		backlog = h.get_backlog_issues(project_df)
-
-		grouped_users = project_df.groupby("assignee.name")
 
 		number_of_topics, alpha, beta = h.get_hyperparameters(project)
 
@@ -98,30 +95,48 @@ def save_results():
 		
 		backlog = lda.add_topic_vector_to_baclog_issues(backlog, lda_model, dictionary, number_of_topics)
   
-		generations = get_generations().get(project)
-
+		lda_end_time = time.time()
+  
+		lda_execution_time = lda_end_time - lda_start_time
+  
 		results = {
 			'project': [],
 			'assignee': [],
 			'hypervolume': [],
+			'opt_execution_time': [],
+			'lda_execution_time': [],
+			'velocity': []
 			}
 
-		pbar = tqdm.tqdm(len(grouped_users))
+		grouped_users = done.groupby("assignee.name")
 
 		for user, user_df in grouped_users: 
+			print(user)
 		
 			if (len(user_df.index) < 1):
 				continue
+   
+			velocity = h.get_velocity(user_df)
+   
+			if velocity < 1:
+				continue
+
+			opt_start_time = time.time()
 
 			vector = lda.get_user_experience_topic_vector(user_df, lda_model, dictionary, number_of_topics)
 
-			hypervolume = get_hypervolume(backlog, vector, 15, generations)
+			hypervolume = get_hypervolume(backlog, vector, velocity)
+   
+			opt_end_time = time.time()
+
+			opt_execution_time = opt_end_time - opt_start_time
 
 			results['project'].append(project)
 			results['assignee'].append(user)
 			results['hypervolume'].append(hypervolume)
-			pbar.update(1)
-
+			results['velocity'].append(velocity)
+			results['opt_execution_time'].append(opt_execution_time)
+			results['lda_execution_time'].append(lda_execution_time)
 						
 		pd.DataFrame(results).to_csv('./thesis-api/algorithm/validation/' + project.lower() + '.csv', index=False)
 
@@ -138,5 +153,3 @@ def combine_validation():
   
 if __name__ == '__main__':
 	save_results()
-	# combine_validation()
-	pass
